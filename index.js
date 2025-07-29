55206ef3aa1c8c78d8ead436cae76d343d8447e6
nancygv <110550654+nancygv@users.noreply.github.com>
2025-07-29 11:17:29 -0600

    fix: ensure correct calculation of age difference for majority and remaining years until 100

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ try {
   const apellido = core.getInput('apellido');
   const edad = parseInt(core.getInput('edad'), 10);
 
-  const mayorEdad = edad - 18; // Diferencia entre edad actual y mayoría de edad
+  const mayorEdad = Math.max(0, edad - 18);
   const tiempo = 100 - edad;
 
   console.log(`${nombre} ${apellido} cumplio la mayoria de edad hace ${mayorEdad} años y le faltan ${tiempo} años para cumplir 100 años`);
